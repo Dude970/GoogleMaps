be5313658bc1ab2c0de6f480f802658685c4411c
Raghu <raghu.sunkari@rampgroup.com>
2023-04-11 16:22:50 +0530

    continue part 2

--- app/src/main/java/com/example/GoogleMaps/data/repository/StockRepositoryImpl.kt
+++ app/src/main/java/com/example/GoogleMaps/data/repository/StockRepositoryImpl.kt
@@ -6,8 +6,12 @@ import com.example.GoogleMaps.data.remote.StockApi
 import com.example.GoogleMaps.domain.model.CompanyListing
 import com.example.GoogleMaps.domain.repository.StockRepository
 import com.example.GoogleMaps.util.Resource
+import com.opencsv.CSVReader
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
+import retrofit2.HttpException
+import java.io.IOException
+import java.io.InputStreamReader
 import javax.inject.Inject
 import javax.inject.Singleton
 
@@ -28,6 +32,24 @@ class StockRepositoryImpl @Inject constructor(
             val localListings = dao.searchCompanyListings(query)
 
             emit(Resource.Success(data = localListings.map { it.toCompanyListing() }))
+
+            val isDbEmpty = localListings.isEmpty() && query.isBlank()
+            val shouldJustLoadFromCache = !isDbEmpty && !fetchFromRemote
+            if (shouldJustLoadFromCache) {
+                emit(Resource.Loading(false))
+                return@flow
+            }
+            val remoteListings = try {
+                val response = api.getListings()
+                response.byteStream()
+                val csvReader = CSVReader(InputStreamReader(response.byteStream()))
+            } catch (e: IOException) {
+                e.printStackTrace()
+                emit(Resource.Error("Couldn't load data"))
+            } catch (e: HttpException) {
+                emit(Resource.Error("Couldn't load data"))
+
+            }
         }
 
     }
